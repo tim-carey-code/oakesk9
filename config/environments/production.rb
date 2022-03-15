f287d360ac17c95e67bca9b97613fc1cd71deee4
Timothy Carey <timcarey1989@gmail.com>
2022-03-15 03:13:31 -0400

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,21 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { host: 'https://oakesk9.herokuapp.com/' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'disinhumepa@gmail.com',
+    password:             ENV['GMAIL_USER_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
+
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
