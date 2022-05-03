2b41b79ad309648c6b1af166e9f4226a1719c93b
Timothy Carey <timcarey1989@gmail.com>
2022-05-03 11:19:44 -0400

    added foreign keys

---
+++ db/migrate/20220503151820_update_foreign_keys.rb
@@ -0,0 +1,9 @@
+class UpdateForeignKeys < ActiveRecord::Migration[7.0]
+  def change
+    remove_foreign_key :comments, :blogs 
+    remove_foreign_key :comments, :users 
+
+    add_foreign_key :comments, :blogs, on_delete: :cascade 
+    add_foreign_key :comments, :users, on_delete: :cascade
+  end
+end
