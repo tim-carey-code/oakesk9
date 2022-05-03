809eed77000000b82f682475c6e966108813e016
Timothy Carey <timcarey1989@gmail.com>
2022-05-03 11:21:19 -0400

    added more foreign keys

---
+++ db/migrate/20220503152037_add_blog_foreign_key.rb
@@ -0,0 +1,5 @@
+class AddBlogForeignKey < ActiveRecord::Migration[7.0]
+  def change
+    add_foreign_key :blogs, :users, on_delete: :cascade
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_151820) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_03_152037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,6 +92,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_03_151820) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "blogs", "users", on_delete: :cascade
   add_foreign_key "comments", "blogs", on_delete: :cascade
   add_foreign_key "comments", "users", on_delete: :cascade
 end
