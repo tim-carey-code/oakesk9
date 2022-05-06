c3cfda94e2871ea8688e6e53930036504ef359f6
Timothy Carey <timcarey1989@gmail.com>
2022-05-06 15:01:23 -0400

    gcs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = {host: "https://oakesk9.herokuapp.com/"}
+  config.action_mailer.default_url_options = {host: "http://www.oakesk9training.com"}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
